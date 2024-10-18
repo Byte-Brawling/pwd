cbe70706bb7aa0c87e2628010296d5a46581916d
AddonayOsoro <oaddonay@gmail.com>
2024-10-18 16:12:46 +0300

    made some changes in the backend

--- server/main.py
+++ server/main.py
@@ -38,7 +38,7 @@ app.include_router(conversation.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Ideally, specify your extension's origin
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
